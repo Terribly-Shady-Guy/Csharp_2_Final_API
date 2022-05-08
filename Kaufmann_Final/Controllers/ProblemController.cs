68639dd13c7e76b6973995f388005e82c0d1700d
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-05-07 22:31:47 -0500

    Add project files.

---
+++ Kaufmann_Final/Controllers/ProblemController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kaufmann_Final.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProblemController : ControllerBase
+    {
+        [HttpPost]
+        public List<Dictionary<string, string>> AddToDictionaries(List<KeyValuePair<string, string>> keyValuePairs)
+        {
+            var keyValues = new Dictionary<string, string>(keyValuePairs.Count);
+            var keyCount = new Dictionary<string, string>(keyValuePairs.Count / 2);
+
+            foreach (var keyValue in keyValuePairs)
+            {
+                if (!keyValues.ContainsKey(keyValue.Key))
+                {
+                    keyValues.Add(keyValue.Key, keyValue.Value);
+                }
+                else if (!keyCount.ContainsKey(keyValue.Key))
+                {
+                    keyCount.Add(keyValue.Key, "2");
+                }
+                else
+                {
+                    int count = int.Parse(keyCount[keyValue.Key]);
+                    ++count;
+                    keyCount[keyValue.Key] = count.ToString();
+                }
+            }
+
+            var dictionaries = new List<Dictionary<string, string>>
+            {
+                keyValues,
+                keyCount
+            };
+
+            return dictionaries;
+        }
+    }
+}
---
+++ Kaufmann_FinalTests/Controllers/ProblemControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kaufmann_Final.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaufmann_Final.Controllers.Tests
+{
+    [TestClass()]
+    public class ProblemControllerTests
+    {
+        private readonly List<KeyValuePair<string, string>> _testKeyValues = new()
+        {
+            new KeyValuePair<string, string>("doge", "coin"),
+            new KeyValuePair<string, string>("abc", "def"),
+            new KeyValuePair<string, string>("12k", "qrs"),
+            new KeyValuePair<string, string>("doge", "coin"),
+            new KeyValuePair<string, string>("abc", "def"),
+            new KeyValuePair<string, string>("farmnadc", "qrescs"),
+            new KeyValuePair<string, string>("abc", "def")
+        };
+
+        [TestMethod()]
+        public void TwoDuplicateKeysTest()
+        {
+            var controller = new ProblemController();
+
+            var dictionaries = controller.AddToDictionaries(_testKeyValues);
+
+            string expected = "2";
+            string result = dictionaries[1]["doge"];
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ThreeDuplicateKeysTest()
+        {
+            var controller = new ProblemController();
+
+            var dictionaries = controller.AddToDictionaries(_testKeyValues);
+
+            string expected = "3";
+            string result = dictionaries[1]["abc"];
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
