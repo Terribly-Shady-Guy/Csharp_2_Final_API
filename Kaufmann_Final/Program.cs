32ae0795f09a27c17132207a55490f87e0ad23f0
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-05-09 19:40:40 -0500

    created dbcontext and models

--- Kaufmann_Final/Program.cs
+++ Kaufmann_Final/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//configure EF core to query db.
+builder.Services.AddDbContext<Kaufmann_Final.Data.Kaufmann_FinaldbContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("Kaufmann_FinalDB"));
+    });
 
 var app = builder.Build();
 
