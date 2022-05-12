98211bd8d69d9b361565646f8f9afb102536f605
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-05-12 18:57:57 -0500

    Added unit tests

---
+++ Kaufmann_FinalTests/Controllers/DriversControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kaufmann_Final.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Kaufmann_FinalTests;
+
+namespace Kaufmann_Final.Controllers.Tests
+{
+    [TestClass()]
+    public class DriversControllerTests
+    {
+        [TestMethod()]
+        public void GetDriverGoodDataTest()
+        {
+            var controller = new DriversController(TestDBContextCreator.CreateTestContext());
+            var mockData = new DriverLookupModel
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                LicensePlate = "ABC123",
+                SSN = "123-45-6789"
+            };
+
+            var data = controller.GetDriver(mockData);
+
+            Assert.IsInstanceOfType(data, typeof(OkObjectResult));
+        }
+
+        [TestMethod()]
+        public void GetDriverBadDataTest()
+        {
+            var controller = new DriversController(TestDBContextCreator.CreateTestContext());
+            var mockData = new DriverLookupModel
+            {
+                FirstName = "Bad",
+                LastName = "Data",
+                LicensePlate = "ABC123",
+                SSN = "123-45-6789"
+            };
+
+            var data = controller.GetDriver(mockData);
+
+            Assert.IsInstanceOfType(data, typeof(NotFoundResult));
+        }
+
+        [TestMethod()]
+        public void AddDriverWithDuplicateKeyTest()
+        {
+            var controller = new DriversController(TestDBContextCreator.CreateTestContext());
+            var mockData = new NewDriver
+            {
+                DriverLicenseNumber = "M112233445566",
+                FirstName = "John",
+                LastName = "Doe",
+                SocialSecurity = "123-45-6789",
+                DateOfBirth = DateTime.Parse("1996-05-25")
+            };
+
+            var result = controller.AddNewDriver(mockData).Result;
+
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+        }
+    }
+}
---
+++ Kaufmann_FinalTests/TestDBContextCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Kaufmann_Final.Data;
+
+namespace Kaufmann_FinalTests
+{
+    internal static class TestDBContextCreator
+    {
+        private const string connectionString = "Data Source=LAPTOP-05I8GA19;Initial Catalog=Kaufmann_FinalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public static Kaufmann_FinaldbContext CreateTestContext() => new(
+            new DbContextOptionsBuilder<Kaufmann_FinaldbContext>()
+            .UseSqlServer(connectionString)
+            .Options);
+    }
+}
